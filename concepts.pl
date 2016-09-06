ff6472fe836bc1dc69ebb417b1372d6ddf36bb90
DangerBlack <danger.recheng@hotmail.it>
2016-09-07 01:13:51 +0200

    Add more traces for debug

--- concepts.pl
+++ concepts.pl
@@ -15,61 +15,84 @@ Tavern
 
 /*Facts*/
 
-kill(Hero,Monster).
-ask(Mage).
-steal(Hero,RareItem).
-reach(City2).
-reach(City3).
+:-discontiguous(found/1).
+:-discontiguous(kill/1).
+:-discontiguous(kill/2).
 
-all_different([City,City2,City3]).
+kill(hero,monster).
+ask(mage).
+ask(hero).
+steal(hero,rareitem).
+reach(city).
+reach(city2).
+reach(tavern).
+ill(king).
 
 /* Hypotesis */
 
 /*Cure a king*/
-/*cure(King) :- ill(King), found(Medicine).
-found(Medicine) :- kill(Hero,Monster).
-found(Medicine) :- ask(Mage).
-*/
+
+cure(king) :-
+    write('[1 ill]'),
+    ill(king),
+    write('[1 found]'),
+    found(medicine).
+found(medicine) :-
+    write('[2 kill]'),
+    kill(hero,monster).
+found(medicine) :-
+    write('[3 ask]'),
+    ask(mage).
+
 /*The Hobbit */
-/*
-found(Treasure) :- found(Map),kill(Hero,Monster).
-found(Map) :- ask(Mage),found(RareItem)
-found(RareItem) :- kill(Hero,Monster).
-*/
+
+found(treasure) :-
+    write('[4 found]'),
+    found(map),
+    write('[4 kill]'),
+    kill(hero,monster).
+found(map) :-
+    write('[5 ask]'),
+    ask(mage),
+    write('[5 found]'),
+    found(rareitem).
+found(rareitem) :-
+    write('[6 kill]'),
+    kill(hero,monster).
+
 /*The Lord of the Ring */
 
-destroy(RareItem) :-
-    write('[1]'),
-    found(RareItem),
-    gain(Party),
-    reach(City),
-    ask(Mage),
-    kill(Hero,Monster),
-    betrayal(Hero).
-reach(City) :-
-    write('[2]'),
-    reach(City2),
-    kill(Hero,Monster),
-    City!=City2.
-reach(City) :-
-    write('[3]'),
-    reach(City2),
-    reach(City3),
-    City!=City2,City2!=City3.
-found(RareItem) :-
-    write('[4]'),
-    ask(Mage).
-gain(Party) :-
-    write('[5]'),
-    reach(City),
-    reach(Tavern),
-    ask(Hero).
-betrayal(Hero) :-
-    write('[6]'),
-    steal(Hero,RareItem).
+destroy(rareitem) :-
+    write('[7 found]'),
+    found(rareitem),
+    write('[7 gain]'),
+    gain(party),
+    write('[7 reach]'),
+    reach(city2),
+    write('[7 ask]'),
+    ask(mage),
+    write('[7 kill]'),
+    kill(hero,monster),
+    write('[7 betrayal]'),
+    betrayal(hero).
+found(rareitem) :-
+    write('[8 ask]'),
+    ask(mage).
+gain(party) :-
+    write('[9 reach]'),
+    reach(city),
+    write('[9 reach]'),
+    reach(tavern),
+    write('[9 ask]'),
+    ask(hero).
+betrayal(hero) :-
+    write('[10 steal]'),
+    steal(hero,rareitem).
 
-
-/* The sword of shannara */
-/*
-kill(Hero,Monster) :- found(RareItem), reach(City), gain(Party).
-*/
+kill(hero,monster) :-
+    write('[11 found]'),
+    found(rareitem),
+    write('[11 reach]'),
+    reach(city),
+    write('[11 gain]'),
+    gain(party).
