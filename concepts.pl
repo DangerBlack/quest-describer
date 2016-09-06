ce5944d925ca311d7dc0769e222a7cb86de7776b
DangerBlack <danger.recheng@hotmail.it>
2016-09-06 23:04:22 +0200

    Refactoring bugfix and some improvment in the logic, something got wrong with 11

--- concepts.pl
+++ concepts.pl
@@ -1,3 +1,4 @@
+/*
 Hero
 Monster
 King
@@ -5,41 +6,70 @@ Child
 Princess
 Treasure
 Medicine
-Mage /*A wise man or woman, like Mage, Witch, Old Elf, Father, Grandfather */
+Mage
 Map
 RareItem
 City
 Tavern
+*/
+
+/*Facts*/
+
+kill(Hero,Monster).
+ask(Mage).
+steal(Hero,RareItem).
+reach(City2).
+reach(City3).
+
+all_different([City,City2,City3]).
+
+/* Hypotesis */
 
 /*Cure a king*/
-cure(King) :- ill(King), found(Medicine).
+/*cure(King) :- ill(King), found(Medicine).
 found(Medicine) :- kill(Hero,Monster).
 found(Medicine) :- ask(Mage).
-
+*/
 /*The Hobbit */
-
+/*
 found(Treasure) :- found(Map),kill(Hero,Monster).
 found(Map) :- ask(Mage),found(RareItem)
 found(RareItem) :- kill(Hero,Monster).
-
+*/
 /*The Lord of the Ring */
 
-destroy(RareItem) :- found(RareItem),gain(Party),reach(City),ask(Mage),kill(Hero,Monster),betrayal(Hero).
-reach(City) :- reach(City),kill(Hero,Monster).
-reach(City) :- reach(City),reach(City).
-found(RareItem) :- ask(Mage).
-gain(Party) :- reach(City),reach(Tavern),ask(Hero).
-betrayal(Hero) :- kill(Hero,Hero).
-betrayal(Hero) :- steal(Hero,RareItem).
+destroy(RareItem) :-
+    write('[1]'),
+    found(RareItem),
+    gain(Party),
+    reach(City),
+    ask(Mage),
+    kill(Hero,Monster),
+    betrayal(Hero).
+reach(City) :-
+    write('[2]'),
+    reach(City2),
+    kill(Hero,Monster),
+    City!=City2.
+reach(City) :-
+    write('[3]'),
+    reach(City2),
+    reach(City3),
+    City!=City2,City2!=City3.
+found(RareItem) :-
+    write('[4]'),
+    ask(Mage).
+gain(Party) :-
+    write('[5]'),
+    reach(City),
+    reach(Tavern),
+    ask(Hero).
+betrayal(Hero) :-
+    write('[6]'),
+    steal(Hero,RareItem).
+
 
 /* The sword of shannara */
-
+/*
 kill(Hero,Monster) :- found(RareItem), reach(City), gain(Party).
-
-
-
-
-/*Things that can be done! yeah!*/
-ill(King).
-kill(Hero,Monster).
-ask(Mage).
+*/
